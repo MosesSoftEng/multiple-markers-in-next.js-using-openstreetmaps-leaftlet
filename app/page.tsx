efd47639f386c8b9cccc46fa2827c6676ea6e762
Moses Ngige <moses.soft.eng@gmail.com>
2023-09-26 22:31:28 +0300

    Import MyMap dynamically

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,19 @@
+// Import the `dynamic` function from the `next/dynamic` module.
+import dynamic from "next/dynamic";
+
+// Import the React module.
+import React from "react";
+
+// Define a functional React component named `Home`.
 export default function Home() {
-	return <>Hello</>;
+	// Dynamically import the `MyMap` component.
+	// The loading option provides a loading message.
+	// The ssr option disables server-side rendering.
+	const MyMap = dynamic(() => import("../components/MyMap"), {
+		loading: () => <p>A map is loading</p>,
+		ssr: false,
+	});
+
+	// Render the dynamically imported `MyMap` component.
+	return <MyMap />;
 }
